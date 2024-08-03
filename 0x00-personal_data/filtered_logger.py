e2adc856c1a14b4cc6347871be3385f194bde410
Mr-Ndi <onlythenotes@gmail.com>
2024-08-03 06:29:22 +0200

    whole file

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -27,3 +27,78 @@ def filter_datum(fields, redaction, message, separator):
         lambda match: f"{match.group().split('=')[0]}={redaction}{separator}",
         message
     )
+
+
+
+class RedactingFormatter(logging.Formatter):
+    """
+    Redacting Formatter class
+    """
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        record.msg = filter_datum(
+            self.fields,
+            self.REDACTION,
+            record.getMessage(),
+            self.SEPARATOR
+        )
+        return super().format(record)
+
+
+def get_db():
+    """
+    Returns a connector to the MySQL database.
+    """
+    username = os.getenv("PERSONAL_DATA_DB_USERNAME", "root")
+    password = os.getenv("PERSONAL_DATA_DB_PASSWORD", "")
+    host = os.getenv("PERSONAL_DATA_DB_HOST", "localhost")
+    database = os.getenv("PERSONAL_DATA_DB_NAME")
+    connection = mysql.connector.connect(
+        user=username,
+        password=password,
+        host=host,
+        database=database
+    )
+    return connection
+
+
+def main():
+    """
+    Main function that retrieves and displays user data.
+    """
+    logger = logging.getLogger("user_data")
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
+    stream_handler = logging.StreamHandler()
+    formatter = RedactingFormatter(PII_FIELDS)
+    stream_handler.setFormatter(formatter)
+    logger.addHandler(stream_handler)
+    db = get_db()
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM users;")
+    for row in cursor:
+        # Create a log message with the user data
+        log_message = (
+            f"name = {row[0]}; "
+            f"email = {row[1]}; "
+            f"phone = {row[2]}; "
+            f"ssn = {row[3]}; "
+            f"password = {row[4]}; "
+            f"ip = {row[5]}; "
+            f"last_login = {row[6]}; "
+            f"user_agent = {row[7]};"
+        )
+        logger.info(log_message)
+    cursor.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    main()
