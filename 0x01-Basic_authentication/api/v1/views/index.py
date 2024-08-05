0226f984448073c155550412b46b96e6a7419751
Mr-Ndi <onlythenotes@gmail.com>
2024-08-06 01:26:09 +0200

    V 1.03

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -25,8 +25,9 @@ def stats() -> str:
     stats['users'] = User.count()
     return jsonify(stats)
 
-@app_views.route('/unaothorizes/', methods=['GET'],strict_slashes)
-def unaothorizes() -> str:
+
+@app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
+def unauthorized_request() -> str:
   """
     Routes for testing unauthorised function
   """
