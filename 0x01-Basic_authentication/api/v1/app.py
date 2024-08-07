65ccc9cce39fe02ffabaadc9f0c07c3724066961
Mr-Ndi <onlythenotes@gmail.com>
2024-08-07 05:29:59 +0200

    Unknown version 2

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -22,15 +22,13 @@ def not_found(error) -> str:
 
 
 @app.errorhandler(401)
-def unauthorized(error) -> str:
-    """ Not found handler
+def Unauthorized(error) -> str:
+    """ Unauthorized handler
     """
     return jsonify({"error": "Unauthorized"}), 401
-
-
 @app.errorhandler(403)
-def unauthorized(error) -> str:
-    """ Not found handler
+def Forbidden(error) -> str:
+    """ Forbidden handler
     """
     return jsonify({"error": "Forbidden"}), 403
 
