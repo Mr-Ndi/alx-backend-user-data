d5258f7ebbd0bf20963aff4e92570ce1e4fbee22
Mr-Ndi <onlythenotes@gmail.com>
2024-08-08 06:34:18 +0200

    V 5.5-3

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -12,12 +12,6 @@ import os
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
-# auth = None
-# auth_type = getenv("AUTH_TYPE")
-
-# if auth_type == "Auth":
-#     from api.v1.auth.auth import Auth
-#     auth = Auth()
 
 @app.errorhandler(404)
 def not_found(error) -> str:
