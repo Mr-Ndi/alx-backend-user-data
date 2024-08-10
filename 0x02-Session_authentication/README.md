77ca7cacd4f3ffad25ef7c0eece563eacf708662
Mr-Ndi <onlythenotes@gmail.com>
2024-08-10 21:41:07 +0200

    V 1.0

---
+++ 0x02-Session_authentication/README.md
@@ -0,0 +1,11 @@
+# Learning Objectives
+
+## At the end of this project, I'm expected to be able to explain to anyone, without the help of Google:
+
+    -> What authentication means
+    -> What session authentication means
+    -> What Cookies are
+    -> How to send Cookies
+    -> How to parse Cookies
+
+# Mr Ninshuti Ndiramiye + codes -> :) .
---
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+'''this is the auth module tha will have class and method for authentication'''
+
+
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth:
+    '''def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        return False'''
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        '''function that checks the required authentication'''
+        if path is None:
+            return True
+        if excluded_paths is None or len(excluded_paths) == 0:
+            return True
+        if path in excluded_paths:
+            return False
+        if not path.endswith('/'):
+            path += '/'
+        normalized_excluded_paths = [
+            p if p.endswith('/')
+            else p + '/' for p in excluded_paths
+        ]
+
+        if path in normalized_excluded_paths:
+            return False
+        return True
+
+    '''def authorization_header(self, request=None) -> str:
+        return None'''
+    def authorization_header(self, request=None) -> str:
+        '''defining the authorization header function that
+        takes the request parameter'''
+        if not request:
+            return None
+        if request.headers.get('Authorization') is None:
+            return None
+        return request.headers.get('Authorization')
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        ''' Basic auth and checking current user'''
+        return None
